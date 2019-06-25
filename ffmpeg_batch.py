49dee2c2f5bb2d5b5f2c9499156b8667d34db4b2
Natkeeran <natkeeran@gmail.com>
2019-06-25 16:35:41 -0400

    Update ffmpeg_batch.py

--- ffmpeg_batch.py
+++ ffmpeg_batch.py
@@ -10,7 +10,7 @@ import os
 # In[7]:
 
 
-media_list = glob.glob("/srv/dsu-backup/Backup-Area/fedora-export/nearby-exports/*.mp4")
+media_list = glob.glob("/sourcepath/*.mp4")
 
 
 # In[11]:
@@ -19,8 +19,7 @@ media_list = glob.glob("/srv/dsu-backup/Backup-Area/fedora-export/nearby-exports
 for file_path in media_list:
     head, tail = os.path.split(file_path)
     file_name = os.path.splitext(tail)[0]
-    #cmd = "ffmpeg -hide_banner -i " + file_path + " -acodec alac /srv/dsu-backup/Backup-Area/fedora-export/nearby_audio/" + file_name + ".m4a"
-    cmd = "ffmpeg -hide_banner -i " + file_path + " -c:v libx264 -profile:v high -c:a aac -profile:a aac_low -strict -2 /srv/dsu-backup/Backup-Area/fedora-export/nearby_video/" + file_name + ".mp4"
+    cmd = "ffmpeg -hide_banner -i " + file_path + " -c:v libx264 -profile:v high -c:a aac -profile:a aac_low -strict -2 /targetpath/" + file_name + ".mp4"
 
     os.system(cmd)
 
