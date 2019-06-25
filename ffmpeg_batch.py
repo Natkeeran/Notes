8824f1db16e0d531ba0cc807922c6bab578b8533
Natkeeran <natkeeran@gmail.com>
2019-06-25 16:35:12 -0400

    Update ffmpeg_batch.py

--- ffmpeg_batch.py
+++ ffmpeg_batch.py
@@ -1,5 +1,3 @@
-  GNU nano 2.5.3            File: ffmpeg_batch.py                               
-
 # coding: utf-8
 
 # In[6]:
@@ -12,7 +10,7 @@ import os
 # In[7]:
 
 
-media_list = glob.glob("/srv/dsu-backup/Backup-Area/fedora-export/nearby-export$
+media_list = glob.glob("/srv/dsu-backup/Backup-Area/fedora-export/nearby-exports/*.mp4")
 
 
 # In[11]:
@@ -21,11 +19,9 @@ media_list = glob.glob("/srv/dsu-backup/Backup-Area/fedora-export/nearby-export$
 for file_path in media_list:
     head, tail = os.path.split(file_path)
     file_name = os.path.splitext(tail)[0]
-    #cmd = "ffmpeg -hide_banner -i " + file_path + " -acodec alac /srv/dsu-back$
-    cmd = "ffmpeg -hide_banner -i " + file_path + " -c:v libx264 -profile:v hig$
+    #cmd = "ffmpeg -hide_banner -i " + file_path + " -acodec alac /srv/dsu-backup/Backup-Area/fedora-export/nearby_audio/" + file_name + ".m4a"
+    cmd = "ffmpeg -hide_banner -i " + file_path + " -c:v libx264 -profile:v high -c:a aac -profile:a aac_low -strict -2 /srv/dsu-backup/Backup-Area/fedora-export/nearby_video/" + file_name + ".mp4"
 
     os.system(cmd)
-    
+
     print(file_name)
-
-
